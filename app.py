e4120a3cab3e6f5c39fc8472a686e0c5dd17f951
iSiddharth20 <kekresiddharth20@gmail.com>
2024-07-21 08:38:32 +0000

    Dry Run to Check Ollama, using Classes and Objects

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from llama_index.llms.ollama import Ollama
 
-llm = Ollama(model="llama3")
+class OllamaResponse:
+    def __init__(self, model_name=None):
+        ''' If No Model is Specified, Ollama will assume Meta-Llama-3-8B '''
+        self.model_name = "llama3" if not model_name else model_name
+        self.llm = Ollama(model=self.model_name)
+    
+    def set_query(self, query):
+        ''' Get Query from Client, Set Query for Further Processing '''
+        self.query = query
+        # You can add Query formatting code here
+    
+    def get_response_from_ollama(self):
+        ''' Send Query to Ollama, Get Response from Ollama '''
+        self.response = self.llm.complete(self.query)
+        # You can add Response formatting code here
+    
+    def post_response(self):
+        ''' Send Response to Client '''
+        return self.response
 
-query = r"Hello! You up for a quick chat?"
 
-response = llm.complete(query)
+if __name__ == '__main__':
 
-print(response)
+    test_run = OllamaResponse()
+
+    query = r"Hello! You up for a quick chat?"
+
+    test_run.set_query(query)
+    test_run.get_response_from_ollama()
+
+    response = test_run.post_response()
+    print(response)
+
