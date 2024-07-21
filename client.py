0e73f61f01cf799933f291e88c3fcd7abbc1421e
iSiddharth20 <kekresiddharth20@gmail.com>
2024-07-21 10:13:21 +0000

    Added .env for Server IP and Port Number

--- client.py
+++ client.py
@@ -1,11 +1,18 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+server_ip_address = os.getenv('SERVER_IP_ADDRESS')
+server_port_number = os.getenv('SERVER_PORT_NUMBER')
+server_ip_and_port = f"{server_ip_address}:{server_port_number}"
 
 # Send Query to Server
 query = r"Hello! You up for a quick chat?"
-requests.get(f"http://127.0.0.1:8000/query/", params={'query': query})
+requests.get(f"http://{server_ip_and_port}/query/", params={'query': query})
 
 # Obtain Response from Server
-post_response = requests.post("http://127.0.0.1:8000/response/")
+post_response = requests.post(f"http://{server_ip_and_port}/response/")
 
 # Display Obtained Response
 response_data = post_response.json()
